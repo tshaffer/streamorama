b024909e6775dd282574bf65ce7c356b57ef6524
Ted Shaffer <ted@brightsign.biz>
2016-11-19 11:50:21 -0800

    Encoder source

--- streamingController/src/components/encoder.js
+++ streamingController/src/components/encoder.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import {Component} from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import { Component } from 'react';
+// import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
 
 class Encoder extends Component {
 
-    componentWillMount() {
+    constructor(props) {
+        super(props);
+        this.state = { value: 1 };
     }
+
+    // <RaisedButton label="Default" />
+
+    handleChange(event, index, value) {
+        this.setState({value});
+    }
+
+
+// <p>Source</p>
 
     render() {
 
         return (
-            <RaisedButton label="Default" />
+            <div>
+                <div>
+                    <div>
+                        <SelectField
+                            floatingLabelText="Source"
+                            value={this.state.value}
+                            onChange={this.handleChange.bind(this)}
+                        >
+                            <MenuItem value={1} primaryText="HDMI" />
+                            <MenuItem value={2} primaryText="File" />
+                        </SelectField>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
