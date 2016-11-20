037da2f343dffa23b562d49683d66fc97229ea43
Ted Shaffer <ted@brightsign.biz>
2016-11-20 07:33:26 -0800

    mods to server to make it match bangServer. works at this point.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,37 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 console.log("launch streamorama server app");
 
 app.use('/', express.static(path.join(__dirname, '../streamingController/')));
 
-function setEncoderParams(encoderParams) {
+app.get('/setEncoderParams', function(req, res) {
 
     console.log("setEncoderParams invoked");
-    console.log(encoderParams);
+    res.set('Access-Control-Allow-Origin', '*');
+
+    // check what's in req.query
+    // var name = req.query.name;
+    //
+    // var promise = dbController.getBSNPresentation(name);
+    // promise.then(function(bsnPresentation) {
+    //     var response = {};
+    //     response.bsnPresentation = bsnPresentation;
+    //     res.send(response);
+    // }, function(err) {
+    //     res.send("fail");
+    // });
+
+    // console.log(encoderParams);
 
     // read existing file
     var str = fs.readFileSync("encoders.json", "ascii");
@@ -26,7 +44,9 @@ function setEncoderParams(encoderParams) {
     // write updated file
     var encodersStr = JSON.stringify(encoders, null, '\t');
     fs.writeFileSync("encoders.json", encodersStr);
-}
+
+});
+
 
 function send200(response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -34,41 +54,33 @@ function send200(response) {
     response.end();
 }
 
-var server = http.createServer(function (request, response) {
+// var server = http.createServer(function (request, response) {
+//
+//     console.log("server callback invoked");
+//
+//     var responseData = {};
+//     responseData.serverResponse = response;
+//
+//     var filePath = false;
+//
+//     parsedUrl = url.parse(request.url, true);
+//
+//     console.log("request url");
+//     console.log(request.url);
+//     console.log("parsed url pathname");
+//     console.log(parsedUrl.pathname);
+//     console.log("parsed url query");
+//     console.log(parsedUrl.query);
+//
+//     send200(response);
+// });
+//
+// server.listen(8080, function () {
+//     console.log("Server listening on port 8080.");
+// });
+//
 
-    console.log("server callback invoked");
+console.log("streamorama server listening on port 8080");
 
-    var responseData = {};
-    responseData.serverResponse = response;
-
-    var filePath = false;
-
-    parsedUrl = url.parse(request.url, true);
-
-    console.log("request url");
-    console.log(request.url);
-    console.log("parsed url pathname");
-    console.log(parsedUrl.pathname);
-    console.log("parsed url query");
-    console.log(parsedUrl.query);
-
-    // if (parsedUrl.pathname === '/setEncoderParams' || request.url === '/') {
-    //     setEncoderParams(parsedUrl.query);
-    // }
-    send200(response);
-
-    // else if (request.url == '/') {                                      // default to index.html
-    //     filePath = 'public/index.html';
-    // } else {                                                            // serve static file
-    //     parsedUrl = url.parse(request.url);
-    //     filePath = "public" + parsedUrl.pathname;
-    // }
-    // var absPath = './' + filePath;
-    // console.log("absPath = " + absPath);
-    // serveStatic(response, cache, absPath);
-});
-
-server.listen(8080, function () {
-    console.log("Server listening on port 8080.");
-});
-
+var port = process.env.PORT || 8080;
+app.listen(port);
