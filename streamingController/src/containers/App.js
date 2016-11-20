9f2c59bcc298eb2eead7ff62092c03e0198b0084
Ted Shaffer <ted@brightsign.biz>
2016-11-19 16:45:57 -0800

    now done properly

--- streamingController/src/containers/App.js
+++ streamingController/src/containers/App.js
@@ -1,62 +1,15 @@
 import React, { Component } from 'react';
-import { hashHistory } from 'react-router';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
 
-// import Landing from '../components/landing';
-
-// import EncoderContainer from '../containers/encoderContainer';
-
-// return (
-//     <MuiThemeProvider>
-//         <Landing/>
-//     </MuiThemeProvider>
-// );
+import Landing from '../components/landing';
 
 class App extends Component {
 
-    handleAddEncoder() {
-        hashHistory.push('/addEncoder');
-    }
-
-    handleAssignDecoderToEncoder() {
-        hashHistory.push('/assignDecoderToEncoder');
-    }
-
-    handleViewBrightSigns() {
-        hashHistory.push('/viewBrightSigns');
-    }
-
     render() {
-
-        let self = this;
-
-        const style = {
-            margin: 12,
-        };
-
         return (
             <MuiThemeProvider>
-                <div>
-                    <RaisedButton
-                        onClick={this.handleAddEncoder.bind(this)}
-                        label="Add Encoder"
-                        style={style}
-                    />
-
-                    <RaisedButton
-                        onClick={this.handleAssignDecoderToEncoder.bind(this)}
-                        label="Assign Decoder to Encoder"
-                        style={style}
-                    />
-
-                    <RaisedButton
-                        onClick={this.handleViewBrightSigns.bind(this)}
-                        label="View BrightSigns"
-                        style={style}
-                    />
-                </div>
+                <Landing/>
             </MuiThemeProvider>
         );
     }
