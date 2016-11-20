deb1e7de099a75170be1312f060ec79f8841df41
Ted Shaffer <ted@brightsign.biz>
2016-11-19 16:42:32 -0800

    it now works - now I’ll try to do it right.

--- streamingController/src/components/encoder.js
+++ streamingController/src/components/encoder.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
@@ -117,165 +119,170 @@ class Encoder extends Component {
         };
 
         return (
-            <div>
-                <div id="encoderStreamOptions">
-                    <div>
-                        <SelectField
-                            floatingLabelText="Source"
-                            value={this.state.sourceValue}
-                            onChange={this.handleSourceChange.bind(this)}
-                        >
-                            <MenuItem value={0} primaryText="HDMI"/>
-                            <MenuItem value={1} primaryText="File"/>
-                        </SelectField>
-                    </div>
-                    <p
-                        className="sectionHeader">
-                        Stream Options
-                    </p>
-                    <div>
-                        <SelectField
-                            floatingLabelText="Stream Type"
-                            value={this.state.streamTypeValue}
-                            onChange={this.handleStreamTypeChange.bind(this)}
-                        >
-                            <MenuItem value={0} primaryText="Unicast"/>
-                            <MenuItem value={1} primaryText="Multicast"/>
-                        </SelectField>
-                    </div>
-                    <div>
-                        <SelectField
-                            floatingLabelText="Protocol"
-                            value={this.state.protocolValue}
-                            onChange={this.handleProtocolChange.bind(this)}
-                        >
-                            <MenuItem value={0} primaryText="UDP"/>
-                            <MenuItem value={1} primaryText="RTP"/>
-                        </SelectField>
-                        <TextField
-                            ref={(c) => {
-                                self.ttlField = c;
-                            }}
-                            defaultValue="1"
-                            floatingLabelText="TTL"
-                            floatingLabelFixed={true}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            ref={(c) => {
-                                self.destinationAddressField = c;
-                            }}
-                            defaultValue="239.194.0.2"
-                            floatingLabelText="Destination Address"
-                            floatingLabelFixed={true}
-                        />
-                        <TextField
-                            ref={(c) => {
-                                self.portField = c;
-                            }}
-                            defaultValue="1234"
-                            floatingLabelText="Port"
-                            floatingLabelFixed={true}
-                        />
 
-                    </div>
-                    <div>
-                        <p>
-                            <span>
-                                Maximum Bitrate:
-                            </span>
-                            <span>
-                                {this.state.maxBitrateValue.toFixed(1)}
-                            </span>
-                            <span>
-                                Mb/s
-                            </span>
+            <MuiThemeProvider>
+
+                <div>
+                    <div id="encoderStreamOptions">
+                        <div>
+                            <SelectField
+                                floatingLabelText="Source"
+                                value={this.state.sourceValue}
+                                onChange={this.handleSourceChange.bind(this)}
+                            >
+                                <MenuItem value={0} primaryText="HDMI"/>
+                                <MenuItem value={1} primaryText="File"/>
+                            </SelectField>
+                        </div>
+                        <p
+                            className="sectionHeader">
+                            Stream Options
                         </p>
-                        <Slider
-                            ref={(c) => {
-                                self.maxBitrateField = c;
-                            }}
-                            defaultValue={6.0}
-                            min={1.0}
-                            max={40.0}
-                            onChange={this.handleMaxBitrateChange.bind(this)}
-                        />
+                        <div>
+                            <SelectField
+                                floatingLabelText="Stream Type"
+                                value={this.state.streamTypeValue}
+                                onChange={this.handleStreamTypeChange.bind(this)}
+                            >
+                                <MenuItem value={0} primaryText="Unicast"/>
+                                <MenuItem value={1} primaryText="Multicast"/>
+                            </SelectField>
+                        </div>
+                        <div>
+                            <SelectField
+                                floatingLabelText="Protocol"
+                                value={this.state.protocolValue}
+                                onChange={this.handleProtocolChange.bind(this)}
+                            >
+                                <MenuItem value={0} primaryText="UDP"/>
+                                <MenuItem value={1} primaryText="RTP"/>
+                            </SelectField>
+                            <TextField
+                                ref={(c) => {
+                                    self.ttlField = c;
+                                }}
+                                defaultValue="1"
+                                floatingLabelText="TTL"
+                                floatingLabelFixed={true}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                ref={(c) => {
+                                    self.destinationAddressField = c;
+                                }}
+                                defaultValue="239.194.0.2"
+                                floatingLabelText="Destination Address"
+                                floatingLabelFixed={true}
+                            />
+                            <TextField
+                                ref={(c) => {
+                                    self.portField = c;
+                                }}
+                                defaultValue="1234"
+                                floatingLabelText="Port"
+                                floatingLabelFixed={true}
+                            />
+
+                        </div>
+                        <div>
+                            <p>
+                                <span>
+                                    Maximum Bitrate:
+                                </span>
+                                <span>
+                                    {this.state.maxBitrateValue.toFixed(1)}
+                                </span>
+                                <span>
+                                    Mb/s
+                                </span>
+                            </p>
+                            <Slider
+                                ref={(c) => {
+                                    self.maxBitrateField = c;
+                                }}
+                                defaultValue={6.0}
+                                min={1.0}
+                                max={40.0}
+                                onChange={this.handleMaxBitrateChange.bind(this)}
+                            />
+                        </div>
+                    </div>
+                    <div id="encoderEncodingOptions">
+                        <div>
+                            <TextField
+                                ref={(c) => {
+                                    self.serialNumberField = c;
+                                }}
+                                defaultValue=""
+                                floatingLabelText="Serial Number"
+                                floatingLabelFixed={true}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                defaultValue=""
+                                floatingLabelText="File Name"
+                                floatingLabelFixed={true}
+                            />
+                        </div>
+                        <div>
+                            <RaisedButton
+                                onClick={this.handleStartStreaming.bind(this)}
+                                label="Start"
+                                style={style}
+                            />
+                            <RaisedButton label="Stop" style={style} />
+                        </div>
+                        <p
+                            className="sectionHeader">
+                            Encoding Options
+                        </p>
+                        <div>
+                            <SelectField
+                                floatingLabelText="Video Codec"
+                                value={this.state.videoCodecValue}
+                                onChange={this.handleVideoCodecChange.bind(this)}
+                            >
+                                <MenuItem value={0} primaryText="H264"/>
+                            </SelectField>
+                        </div>
+                        <div>
+                            <SelectField
+                                floatingLabelText="Video Format"
+                                value={this.state.videoFormatValue}
+                                onChange={this.handleVideoFormatChange.bind(this)}
+                            >
+                                <MenuItem value={0} primaryText="720p30"/>
+                                <MenuItem value={1} primaryText="1080i60"/>
+                            </SelectField>
+                        </div>
+                        <div>
+                            <p>
+                                <span>
+                                    Bitrate:
+                                </span>
+                                <span>
+                                    {this.state.bitrateValue.toFixed(1)}
+                                </span>
+                                <span>
+                                    Mb/s
+                                </span>
+                            </p>
+                            <Slider
+                                ref={(c) => {
+                                    self.bitrateField = c;
+                                }}
+                                defaultValue={2.0}
+                                min={1.0}
+                                max={40.0}
+                                onChange={this.handleBitrateChange.bind(this)}
+                            />
+                        </div>
                     </div>
                 </div>
-                <div id="encoderEncodingOptions">
-                    <div>
-                        <TextField
-                            ref={(c) => {
-                                self.serialNumberField = c;
-                            }}
-                            defaultValue=""
-                            floatingLabelText="Serial Number"
-                            floatingLabelFixed={true}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            defaultValue=""
-                            floatingLabelText="File Name"
-                            floatingLabelFixed={true}
-                        />
-                    </div>
-                    <div>
-                        <RaisedButton
-                            onClick={this.handleStartStreaming.bind(this)}
-                            label="Start"
-                            style={style}
-                        />
-                        <RaisedButton label="Stop" style={style} />
-                    </div>
-                    <p
-                        className="sectionHeader">
-                        Encoding Options
-                    </p>
-                    <div>
-                        <SelectField
-                            floatingLabelText="Video Codec"
-                            value={this.state.videoCodecValue}
-                            onChange={this.handleVideoCodecChange.bind(this)}
-                        >
-                            <MenuItem value={0} primaryText="H264"/>
-                        </SelectField>
-                    </div>
-                    <div>
-                        <SelectField
-                            floatingLabelText="Video Format"
-                            value={this.state.videoFormatValue}
-                            onChange={this.handleVideoFormatChange.bind(this)}
-                        >
-                            <MenuItem value={0} primaryText="720p30"/>
-                            <MenuItem value={1} primaryText="1080i60"/>
-                        </SelectField>
-                    </div>
-                    <div>
-                        <p>
-                            <span>
-                                Bitrate:
-                            </span>
-                            <span>
-                                {this.state.bitrateValue.toFixed(1)}
-                            </span>
-                            <span>
-                                Mb/s
-                            </span>
-                        </p>
-                        <Slider
-                            ref={(c) => {
-                                self.bitrateField = c;
-                            }}
-                            defaultValue={2.0}
-                            min={1.0}
-                            max={40.0}
-                            onChange={this.handleBitrateChange.bind(this)}
-                        />
-                    </div>
-                </div>
-            </div>
+
+            </MuiThemeProvider>
         );
     }
 }
--- streamingController/src/containers/App.js
+++ streamingController/src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { hashHistory } from 'react-router';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 // import Landing from '../components/landing';
 
-import EncoderContainer from '../containers/encoderContainer';
+// import EncoderContainer from '../containers/encoderContainer';
 
 // return (
 //     <MuiThemeProvider>
@@ -14,11 +16,47 @@ import EncoderContainer from '../containers/encoderContainer';
 
 class App extends Component {
 
+    handleAddEncoder() {
+        hashHistory.push('/addEncoder');
+    }
+
+    handleAssignDecoderToEncoder() {
+        hashHistory.push('/assignDecoderToEncoder');
+    }
+
+    handleViewBrightSigns() {
+        hashHistory.push('/viewBrightSigns');
+    }
+
     render() {
+
+        let self = this;
+
+        const style = {
+            margin: 12,
+        };
 
         return (
             <MuiThemeProvider>
-                <Landing/>
+                <div>
+                    <RaisedButton
+                        onClick={this.handleAddEncoder.bind(this)}
+                        label="Add Encoder"
+                        style={style}
+                    />
+
+                    <RaisedButton
+                        onClick={this.handleAssignDecoderToEncoder.bind(this)}
+                        label="Assign Decoder to Encoder"
+                        style={style}
+                    />
+
+                    <RaisedButton
+                        onClick={this.handleViewBrightSigns.bind(this)}
+                        label="View BrightSigns"
+                        style={style}
+                    />
+                </div>
             </MuiThemeProvider>
         );
     }
