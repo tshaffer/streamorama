af57d2400615d40c83113e3119cac5ba30d1c2ed
Ted Shaffer <ted@brightsign.biz>
2016-11-20 16:43:36 -0800

    add second table to see if there are any problems with two tables.

--- controller/client/src/components/brightSigns.js
+++ controller/client/src/components/brightSigns.js
@@ -130,6 +130,39 @@ class BrightSigns extends Component {
                             {encoderRows}
                         </TableBody>
                     </Table>
+
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHeaderColumn>ID</TableHeaderColumn>
+                                <TableHeaderColumn>Name</TableHeaderColumn>
+                                <TableHeaderColumn>Status</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableRowColumn>1</TableRowColumn>
+                                <TableRowColumn>John Smith</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>2</TableRowColumn>
+                                <TableRowColumn>Randal White</TableRowColumn>
+                                <TableRowColumn>Unemployed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>3</TableRowColumn>
+                                <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>4</TableRowColumn>
+                                <TableRowColumn>Steve Brown</TableRowColumn>
+                                <TableRowColumn>Employed</TableRowColumn>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+
                 </div>
 
             </MuiThemeProvider>
